5a9439efb25d5df3d7f88449f921deb9f6271b07
endokai <my1130546419@gmail.com>
2018-12-30 14:28:10 +0800

    修改privilege_token为token

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 
-echo "privilege_token = $pass">>/bin/frps.ini && \
+echo "token = $pass">>/bin/frps.ini && \
 echo "dashboard_pwd = $dashpass">>/bin/frps.ini && \
 frps -c /bin/frps.ini
